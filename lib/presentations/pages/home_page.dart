9454aa7e9db0f1f65bf3c39d21cb21746b399ad6
fluttberry <srvffx@gmail.com>
2022-12-04 17:59:57 +0600

    bmi_app_ongoing

--- lib/presentations/pages/home_page.dart
+++ lib/presentations/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:bmi_app/presentations/widgets/custom_container.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,14 +8,21 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Colors.black,
         title: const Text('BMI Calculator'),
+        centerTitle: true,
       ),
       body: Column(children: [
-        Row(children: [
-          Container(
-            
-          )
-        ],)
+        Row(
+          children: const [
+            CustomContainer(text: 'male', icons: Icons.male),
+            CustomContainer(text: 'female', icons: Icons.female),
+          ],
+        ),
+        const CustomContainer(
+          text: 'height',
+          
+        )
       ]),
     );
   }
---
+++ lib/presentations/widgets/custom_container.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class CustomContainer extends StatelessWidget {
+  final IconData? icons;
+
+  final String? text;
+  const CustomContainer({
+    this.icons,
+    this.text,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: Colors.teal,
+      ),
+      padding: const EdgeInsets.all(50),
+      child: Column(
+        children: [
+          Icon(
+            icons,
+            size: 90,
+          ),
+          Text(
+            text!.toUpperCase(),
+            style: const TextStyle(fontSize: 25),
+          ),
+        ],
+      ),
+    );
+  }
+}
